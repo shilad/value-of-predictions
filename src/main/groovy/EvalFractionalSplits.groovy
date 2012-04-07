f51902c4f4db0f56be6d817d07fdb4e9b3ae2fa8
Shilad Sen <shilad@gmail.com>
2012-04-07 12:04:35 -0500

    Added fractional sampler.

---
+++ src/main/groovy/EvalFractionalSplits.groovy
@@ -0,0 +1,134 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2012 Regents of the University of Minnesota and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+import org.grouplens.lenskit.eval.data.crossfold.RandomOrder
+
+import org.grouplens.lenskit.data.pref.PreferenceDomain
+import org.grouplens.lenskit.eval.metrics.predict.CoveragePredictMetric
+import org.grouplens.lenskit.eval.metrics.predict.MAEPredictMetric
+import org.grouplens.lenskit.eval.metrics.predict.RMSEPredictMetric
+import org.grouplens.lenskit.eval.metrics.predict.NDCGPredictMetric
+import org.grouplens.ratingvalue.MutualInformationMetric
+import org.grouplens.lenskit.RatingPredictor
+import org.grouplens.lenskit.knn.item.ItemItemRatingPredictor
+import org.grouplens.lenskit.baseline.BaselinePredictor
+import org.grouplens.lenskit.baseline.ItemUserMeanPredictor
+import org.grouplens.lenskit.params.UserVectorNormalizer
+import org.grouplens.lenskit.norm.VectorNormalizer
+import org.grouplens.lenskit.norm.BaselineSubtractingNormalizer
+import org.grouplens.lenskit.knn.params.SimilarityDamping
+import org.grouplens.lenskit.knn.params.NeighborhoodSize
+import org.grouplens.lenskit.baseline.BaselineRatingPredictor
+import org.grouplens.ratingvalue.NormalizedMAEMetric
+import org.grouplens.ratingvalue.NormalizedRMSEMetric
+import org.grouplens.ratingvalue.UserFilteringDataSourceWrapper
+
+
+def buildDir = "pwd".execute().text.trim()
+def baselines = [ItemUserMeanPredictor]
+
+def inputDomains = [
+        '2' : new PreferenceDomain(1.0, 2.0, 1.0),
+        '5' : new PreferenceDomain(1.0, 5.0, 1.0),
+        '10' : new PreferenceDomain(0.5, 5.0, 5.0),
+        '101' : new PreferenceDomain(-10.0, 10.0, 0.25)
+]
+
+def predictDomains = [
+        '2' : new PreferenceDomain(1.0, 2.0, 1.0),
+        '5' : new PreferenceDomain(1.0, 5.0, 1.0),
+        '10' : new PreferenceDomain(0.5, 5.0, 0.5),
+        '20' : new PreferenceDomain(0.25, 5.0, 0.25),
+    ]
+
+def datasetConfigs = [
+        'ml-10m' : [
+                'path' : buildDir + '/ml-10M100K/ratings.dat',
+                'delimiter' : '::',
+                'domains' : [
+                        '2' : 3.91 / 3.91,
+                        '5' : 3.91 / 4.09,
+                        '10' : 3.91 / 4.22,
+                ]
+        ],
+        'jester' : [
+                'path' : buildDir + '/jester_ratings.dat',
+                'delimiter' : '::',
+                'domains' : [
+                        '2' : 15.47 / 15.47,
+                        '5' : 15.47 / 16.39,
+                        '10' : 15.47 / 16.55,
+                        '20' : 15.47 / 16.70,
+                        '101' : 15.47 / 17.06
+                ]
+        ]
+]
+
+phony("all") {
+    dsKey = 'ml-10m'
+    //dsKey = 'jester'
+    def dsConfig = datasetConfigs[dsKey]
+    for (int i : [0,1,2,3,4,5,10,15,20,30,40,50]) {
+        int n = (i == 0) ? 1000 : i;
+        for (def entry : dsConfig.domains) {
+            def inKey = entry.key
+            def sample = entry.value
+            def inDomain = inputDomains[inKey]
+            depends trainTest(dsKey + '-' + inKey + '-' + n) {
+                def pathPrefix = "${buildDir}/splits/${dsKey}/${inKey}-${n}";
+                output "${pathPrefix}/eval-results-sampled.csv"
+                for (int j = 0; j < 5; j++) {
+                    dataset {
+                        train dataset {
+                            name "${pathPrefix}/train.${j}.csv"
+                            wrapper new UserFilteringDataSourceWrapper(sample)
+                        }
+                        test "${pathPrefix}/test.${j}.csv"
+                    }
+                }
+
+                metric CoveragePredictMetric
+                metric MAEPredictMetric
+                metric RMSEPredictMetric
+                metric NDCGPredictMetric
+                metric new NormalizedMAEMetric(inDomain)
+                metric new NormalizedRMSEMetric(inDomain)
+
+                for (def pd : predictDomains) {
+                    metric (new MutualInformationMetric("MI-${pd.key}", inDomain, pd.value, false))
+                    metric (new MutualInformationMetric("MI-${pd.key}-corrected", inDomain, pd.value, true))
+                }
+
+                for (bl in baselines) {
+                    algorithm(bl.simpleName.replaceFirst(/Predictor$/, "")) {
+                        setComponent(RatingPredictor, BaselineRatingPredictor)
+                        setComponent(BaselinePredictor, bl)
+                    }
+                }
+
+                algorithm("ItemItem") {
+                    setComponent(RatingPredictor, ItemItemRatingPredictor)
+                    setComponent(BaselinePredictor, ItemUserMeanPredictor)
+                    setComponent(UserVectorNormalizer, VectorNormalizer, BaselineSubtractingNormalizer)
+                    set(SimilarityDamping, 100)
+                    set(NeighborhoodSize, 30);
+                }
+            }
+        }
+    }
+}
--- src/main/java/org/grouplens/ratingvalue/UserFilteringDataSourceWrapper.java
+++ src/main/java/org/grouplens/ratingvalue/UserFilteringDataSourceWrapper.java
@@ -1,21 +1,45 @@
 package org.grouplens.ratingvalue;
 
 import com.google.common.base.Function;
+import org.grouplens.lenskit.data.Event;
 import org.grouplens.lenskit.data.dao.DAOFactory;
 import org.grouplens.lenskit.data.dao.DataAccessObject;
+import org.grouplens.lenskit.data.dao.EventCollectionDAO;
+import org.grouplens.lenskit.data.event.Rating;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserFilteringDataSourceWrapper implements Function<DAOFactory,DAOFactory> {
-    @Override
-    public DAOFactory apply(@Nullable DAOFactory daoFactory) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    private double sampleFraction;
+
+    public UserFilteringDataSourceWrapper(double sampleFraction) {
+        this.sampleFraction = sampleFraction;
     }
 
-    static class FilteringDAOFactory implements DAOFactory {
+    @Override
+    public DAOFactory apply(@Nullable DAOFactory daoFactory) {
+        return new FilteringDAOFactory(daoFactory);
+    }
+
+    class FilteringDAOFactory implements DAOFactory {
+        private DAOFactory delegate;
+
+        FilteringDAOFactory(DAOFactory delegate) {
+            this.delegate = delegate;
+        }
         @Override
         public DataAccessObject create() {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
+            List<Event> sampled = new ArrayList<Event>();
+            DataAccessObject dao = delegate.create();
+            for (Event event : dao.getEvents()) {
+                if (event instanceof Rating && Math.random() > sampleFraction) {
+                    continue;
+                }
+                sampled.add(event);
+            }
+            return new EventCollectionDAO(sampled);
         }
 
         @Override
