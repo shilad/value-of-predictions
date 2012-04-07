e23a535c6d30c95fe8e8c5f7e05709fec34b583b
Shilad Sen <shilad@gmail.com>
2012-04-07 17:50:11 -0500

    Added sample results.

--- res/generate.sh
+++ res/generate.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for dataset in jester ml-10m; do
+#for dataset in jester ml-10m; do
+for dataset in jester; do
+    python2.6 ./src/main/python/aggregate_sample.py splits/$dataset > res/$dataset.sampled.csv
+done
+
+for dataset in jester; do
     python2.6 ./src/main/python/aggregate.py splits/$dataset > res/$dataset.csv
 done
 
